a66ee99a7aed7ca194051318abe2cb394c673025
juancinfante <juaninfantejj@gmail.com>
2025-05-20 10:42:16 -0300

    .

--- src/app/dashboard/reservas/page.jsx
+++ src/app/dashboard/reservas/page.jsx
@@ -11,7 +11,26 @@ import { Separator } from "@/components/components/ui/separator"
 import {
   SidebarTrigger,
 } from "@/components/components/ui/sidebar"
-const page = () => {
+import { DataTable } from './data-table'
+import { columns } from './colums'
+
+async function getData(){
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function page()  {
+
+  const data = await getData()
+
   return (
     <>
       <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
@@ -37,15 +56,14 @@ const page = () => {
         </div>
       </header>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+      <DataTable columns={columns} data={data} />
+        {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
           <div className="bg-muted/50 aspect-video rounded-xl" />
           <div className="bg-muted/50 aspect-video rounded-xl" />
           <div className="bg-muted/50 aspect-video rounded-xl" />
         </div>
-        <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+        <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" /> */}
       </div>
     </>
   )
 }
-
-export default page
